fda950b522f02d8684313110b7763c5a84bfda97
Korivi Sukanya <korivisukanya02@gmail.com>
2026-08-18 23:23:54 +0530

    Initial project setup

---
+++ src/main/java/com/example/productionincidentintelligence/ProductionIncidentIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.productionincidentintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionIncidentIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionIncidentIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productionincidentintelligence/ProductionIncidentIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productionincidentintelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionIncidentIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
